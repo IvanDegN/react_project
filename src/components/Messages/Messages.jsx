8e5a6e4ebdb45d8c804b43c47cc28f22f93a812c
IvanDegN <mauzer.bruno@yandex.ru>
2022-07-22 10:56:21 +0300

    Создана вёрстка для страницы Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,26 @@
-
+import style from './Messages.module.css'
 
 const Messages = () =>
 {
     return (
-        <div>
-            <h1>Messages</h1>
+        <div className={style.dialogs}>
+
+            <div className={style.dialogsItems}>
+                <div className={`${style.dialog} ${style.active}`}>
+                    Ivan
+                </div>
+                <div className={style.dialog}>
+                    Sveta
+                </div>
+                <div className={style.dialog}>
+                    Andrew
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>It looks strange</div>
+            </div>
         </div>
     );
 }
--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,28 +1,27 @@
-import style from './Nav.module.css'
+import './Nav.css'
 import {NavLink} from "react-router-dom";
 
 const Nav = () =>
 {
     return (
-        <nav className={style.nav}>
+        <nav className={'nav'}>
             <div>
-                <NavLink className={style.active} to={'/profile'}>Profile</NavLink>
+                <NavLink    to={'/profile'} >Profile</NavLink>
             </div>
             <div>
-                <NavLink to={'/messages'}>Messages</NavLink>
+                <NavLink   to={'/messages'} >Messages</NavLink>
             </div>
             <div>
-                <NavLink to={'/news'}>News</NavLink>
+                <NavLink  to={'/news'} >News</NavLink>
             </div>
             <div>
-                <NavLink to={'/music'}>Music</NavLink>
+                <NavLink  to={'/music'} >Music</NavLink>
             </div>
 
             <div>
-                <NavLink to={'/settings'}>Settings</NavLink>
+                <NavLink  to={'/settings'} >Settings</NavLink>
             </div>
         </nav>
     );
 }
-
 export default Nav;
